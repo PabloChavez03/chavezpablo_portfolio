e8f45cb4a3f94d9c2f3c282ea9eb15680c935f80
PabloChavez03 <pablooscarchavez@gmail.com>
2023-01-14 14:58:10 -0300

    refact styles

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -7,28 +7,28 @@ import { AiOutlineMinus } from "react-icons/ai";
 
 export default function Bienvenida() {
   return (
-    <div className="flex flex-col sm:grid sm:grid-cols-7 sm:gap-4 place-content-center place-items-center m-auto">
-      <div className="col-start-1 col-end-5 p-7">
-        <div className="mb-4">
-          <div className="mb-4">
-            <h1 className="text-amber-400 font-mono">Divide y conquistarás</h1>
+    <div className="flex flex-col sm:grid sm:grid-cols-7 sm:gap-4 sm:place-content-center sm:place-items-center m-auto">
+      <div className="flex  flex-col md:block items-center justify-between md:col-start-1 md:col-end-5 p-7">
+        <div className="mb-2">
+          <div className="mb-6">
+            <h1 className="pb-2 text-center md:text-left text-amber-400 font-mono">Divide y conquistarás</h1>
           </div>
-          <h1 className="font-serif text-6xl text-gray-200">
+          <h1 className="font-serif text-center md:text-left text-6xl text-gray-200">
             Full Stack Developer
           </h1>
-          <h1 className="font-serif text-6xl text-gray-200">Pablo Chávez</h1>
+          <h1 className="font-serif text-center md:text-left text-6xl text-gray-200">Pablo Chávez</h1>
         </div>
         <div className="mt-4">
-          <span className="text-gray-200">
-            Desarrollador web con experiencia de 9 meses y en crecimiento. Mis
+          <p className="text-center md:text-left text-gray-200">
+            Desarrollador web con un año de experiencia y en crecimiento. Mis
             destrezas residen en diseñar y crear sitios web que faciliten las necesidades de las personas, y otras más.
-          </span>
+          </p>
         </div>
-        <div className="flex flex-row-reverse lg:flex-row justify-end lg:justify-start items-center mt-2 py-2">
+        <div className="flex flex-row-reverse justify-between md:flex-row md:justify-start items-center mt-2 py-2">
           <div className="py-2">
             <ButtonCV text={"Sobre mí"} path={"/about"} />
           </div>
-          <div className="invisible lg:visible lg:ml-9">
+          <div className="hidden md:block lg:ml-9">
             <AiOutlineMinus
               className="text-gray-200 transition-colors duration-75 hover:text-amber-400"
               size={30}
@@ -42,7 +42,7 @@ export default function Bienvenida() {
                 rel="noopener noreferrer"
               >
                 <SiGithub
-                  className="text-gray-200 mx-4 transition-colors duration-75 hover:text-amber-400"
+                  className="text-gray-200 mx-3 md:mx-4 transition-colors duration-75 hover:text-amber-400"
                   size={34}
                 />
               </a>
@@ -54,7 +54,7 @@ export default function Bienvenida() {
                 rel="noreferrer"
               >
                 <SiLinkedin
-                  className="text-gray-200 mx-4 transition-colors duration-75 hover:text-amber-400"
+                  className="text-gray-200 mx-3 md:mx-4 transition-colors duration-75 hover:text-amber-400"
                   size={34}
                 />
               </a>
@@ -66,7 +66,7 @@ export default function Bienvenida() {
                 rel="noopener noreferrer"
               >
                 <SiWhatsapp
-                  className="text-gray-200 mx-4 transition-colors duration-75 hover:text-amber-400"
+                  className="text-gray-200 mx-3 md:mx-4 transition-colors duration-75 hover:text-amber-400"
                   size={34}
                 />
               </a>
@@ -74,7 +74,7 @@ export default function Bienvenida() {
           </div>
         </div>
       </div>
-      <div className="col-start-5 col-end-8 m-5 p-5 max-w-md">
+      <div className="hidden md:block md:col-start-5 md:col-end-8 m-5 p-5 max-w-md">
         <Image src={profilePic} alt={"profile picture"} />
       </div>
     </div>
